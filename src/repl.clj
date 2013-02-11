334ae6752b707a4c8b8f24bdff9c6519e14c8e60
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2013-02-11 14:39:56 +0000

    repl main shows an example

--- src/repl.clj
+++ src/repl.clj
@@ -3,6 +3,5 @@
 
 (defn -main [& args]
   (println "karatsuba multiplication is bound the function *k.
-    Example: (*k 1234567890 9876543210)")
-   (*k 1234567890 9876543210)
-  )
+    Example: (*k 1234567890 9876543210) = " (*k 1234567890 9876543210)
+  ))
