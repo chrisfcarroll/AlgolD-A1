fa2dcf56853228a09cbb62a68e44110dd6cb4044
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2013-02-11 16:31:33 +0000

    repl shows source. Lecture notes

---
+++ notes/Week 1 Notes.md
@@ -0,0 +1,27 @@
+Week 1 Notes
+Vocab
+bigO as granularity level
+
+Divide and conquer
+
+Randomisation
+
+Primitives
+    so fast essentially free (as fast as reading the input)
+
+data structures
+    heaps, trees, hashing
+    hash tables & maps
+
+greedy paradigm
+
+dynamic programming algs
+
+NP complete problems
+
+kleinbery tardos alg design 2006
+
+Dasgupta pap Algorithms 2006
+
+Cormen leiserson rivest stein intro to algo
+
--- src/repl.clj
+++ src/repl.clj
@@ -1,7 +1,15 @@
 (ns repl
-  (:require [week1.karatsuba :refer :all]))
+  (:require
+    [clojure.repl :refer [source]] 
+    [week1.karatsuba :refer :all]))
 
 (defn -main [& args]
-  (println "karatsuba multiplication is bound the function *k.
-    Example: (*k 1234567890 9876543210) = " (*k 1234567890 9876543210)
-  ))
+  (do
+    (ns week1.karatsuba)
+    (println "karatsuba multiplication is bound the function *k.
+    Example: (*k 1234567890 9876543210) = " (*k 1234567890 9876543210) "
+
+    ") 
+    (source *k)
+    (println)
+    (source **k)))
