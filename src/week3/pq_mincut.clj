b1d72aabc66021fb8dd3716e0f4e94ab6cc0faa7
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2013-02-24 16:46:47 +0000

    wip pivoting node-edges to edge nodes

--- src/week3/pq_mincut.clj
+++ src/week3/pq_mincut.clj
@@ -4,19 +4,35 @@
 ; Load the file into a map of nodes->edges and a map of edges->nodes
 (def pq-input-txt (slurp "src/week3/kargerMinCut.txt"))
 
-(defn seq-from-tab-delimited-lines [string]
+(defn mapentries-from-tab-delimited-lines [string]
   (for [line (split-lines string)] 
     (let [[nstring edgesstring] (split line #"\t" 2) 
           node (Integer/parseInt nstring)
           edges (map #(Integer/parseInt %) (split edgesstring #"\t"))]
         [node edges])))
 
-(def pq-input-rows (seq-from-tab-delimited-lines pq-input-txt))
+(def pq-input-rows (mapentries-from-tab-delimited-lines pq-input-txt))
 
-(def pq-input-nodes (into (sorted-map) pq-input-rows))
+(def pq-node-index (into (sorted-map) pq-input-rows))
 
- (def pq-input-edges (into (sorted-map) 
-     (for [[node edges] pq-input-rows] (first (for [edge edges] [edge node])))))
+(def pq-edges-2 
+  (let [pq-edges-builder (transient {} )]
+    (doseq [[node edges] pq-input-rows]
+      (doseq [edge edges]
+        (let [prev-node-for-edge (get pq-edges-builder edge)]
+          (assoc! pq-edges-builder edge (cons node prev-node-for-edge)))))
+    (persistent! pq-edges-builder)))
+
+(defn pq-edges-3 [input-rows]
+    (let [res {0 0}]
+      (reduce 
+        (fn [[node edges] pq-input-rows] 
+          (for [edge edges]
+            (let [prev-node-for-edge (get res edge)]
+              (assoc res edge (cons node prev-node-for-edge)))))
+        input-rows)))
+
+(defn e3 [x] (reduce (fn [res [k v]] (for [e v] (conj res [e k]))) () x))
 
 (comment 
 "The file contains the adjacency list representation of a simple undirected graph. There are 200 vertices labeled 1 to 200. The first column in the file represents the vertex label, and the particular row (other entries except the first column) tells all the vertices that the vertex is adjacent to. So for example, the 6th row looks like : 6  155 56  52  120 ....... 
@@ -24,3 +40,14 @@
 This just means that the vertex with label 6 is adjacent to (i.e., shares an edge with) the vertices with labels 155,56,52,120,......,etc
 
 Your task is to code up and run the randomized contraction algorithm for the min cut problem and use it on the above graph to compute the min cut. (HINT: Note that you'll have to figure out an implementation of edge contractions. Initially, you might want to do this naively, creating a new graph from the old every time there's an edge contraction. But you should also think about more efficient implementations.) (WARNING: As per the video lectures, please make sure to run the algorithm many times with different random seeds, and remember the smallest cut that you ever find.) Write your numeric answer in the space provided. So e.g., if your answer is 5, just type 5 in the space provided.")
+
+(defn merge2
+  "An example implementation of `merge` using transients."
+  [x y]
+  (persistent! (reduce
+                (fn [res [k v]] (assoc! res k v))
+                (transient x)
+                y)))
+
+(defn flatten-1tomany-map-into-kv-pairs [onetomanymap]
+  (reduce (fn [acc [key vals]] (apply conj acc (for [val vals] [key val])) ) () onetomanymap))
--- test/week3/pq_mincut_test.clj
+++ test/week3/pq_mincut_test.clj
@@ -4,8 +4,8 @@
     [week3.pq-mincut :refer :all]
   ))
 
-(deftest seq-from-tab-delimited-lines-should-be-a-seq-of-vectors-of-node-and-seq-of-edges-es
-  (let [s (seq-from-tab-delimited-lines "1\t10\t11\t\n2\t21\t22\t")
+(deftest mapentries-from-tab-delimited-lines-should-be-a-seq-of-vectors-of-node-and-seq-of-edges-es
+  (let [s (mapentries-from-tab-delimited-lines "1\t10\t11\t\n2\t21\t22\t")
         row (first s)]
       (is (= '([1 (10 11)] [2 (21 22)]) s))
       (is (seq? s))
@@ -13,3 +13,7 @@
       (is (int (first row)))
       (is (seq? (second row)))
       (is (int (first (second row))))))
+
+(deftest flatten-1tomany-map-into-kv-pairs-should-be-correct
+  (is (= (reverse '([1 10] [1 12] [2 20] [2 12]))
+         (flatten-1tomany-map-into-kv-pairs {1 [10 12], 2 [20 12]}))))
